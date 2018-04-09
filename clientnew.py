ecf30f7750a46a5e473eb9fdee750ee35bf9931b
Pawan Yadav <pawan.yadav1107@gmail.com>
2018-04-09 08:50:10 +0530

    change recivening end

--- clientnew.py
+++ clientnew.py
@@ -11,21 +11,18 @@ host = '192.168.0.102'
 port = 9999
 
 # connection to hostname on the port. 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 s.connect((host, port))                              
 
 # Receive no more than 1024 bytes
 try:
 	while True:
 		# printscreen_numpy = s.recv(40960000)  
-		data = s.recv(40960000)
-		printscreen_numpy = pickle.loads(data) 
-		# printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8')\
-		# .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))  
-		cv2.imshow('window',printscreen_numpy)
-		if cv2.waitKey(25) & 0xFF == ord('q'):
-			cv2.destroyAllWindows()
-			break
-                 
+		printscreen_pil =  ImageGrab.grab() 
+		printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8').reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
+		data_string = pickle.dumps(printscreen_numpy)
+		s.sendall(data_string) 
 except Exception as e:
 	 print(e)
 finally: 
---
+++ servernew.py
@@ -0,0 +1,37 @@
+# server.py 
+import socket, pickle
+from PIL import ImageGrab
+import cv2
+import numpy as np
+# create a socket object
+serversocket = socket.socket(
+	        socket.AF_INET, socket.SOCK_STREAM) 
+
+# get local machine name                
+host = socket.gethostbyname( '0.0.0.0' )                           
+# host = '192.168.7.242'                         
+
+port = 9999                                           
+
+# bind to the port
+serversocket.bind((host, port))                                  
+
+# queue up to 5 requests
+serversocket.listen(5)                      
+clientsocket,addr = serversocket.accept()                      
+try: 	
+	while True:
+	    data = serversocket.recv(40960000)
+		printscreen_numpy = pickle.loads(data) 
+		# printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8')\
+		# .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))  
+		cv2.imshow('window',printscreen_numpy)
+		if cv2.waitKey(25) & 0xFF == ord('q'):
+			cv2.destroyAllWindows()
+			break 
+except Exception as e:
+	print(e)
+finally: 
+	print("Closing socket and exit") 
+	clientsocket.close()
+ 
